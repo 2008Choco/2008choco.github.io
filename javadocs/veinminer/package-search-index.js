642d3b7fec38ec437193bafb0b5cb4a8b66964a1
Parker Hawke <hawkeboyz2@hotmail.com>
2018-12-20 20:59:50 -0500

    Update VeinMiner's Javadocs to 1.13.0

--- javadocs/veinminer/package-search-index.js
+++ javadocs/veinminer/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"wtf.choco.veinminer"},{"l":"wtf.choco.veinminer.anticheat"},{"l":"wtf.choco.veinminer.api"},{"l":"wtf.choco.veinminer.api.blocks"},{"l":"wtf.choco.veinminer.api.event"},{"l":"wtf.choco.veinminer.pattern"},{"l":"wtf.choco.veinminer.utils"}]
+packageSearchIndex = [{"l":"wtf.choco.veinminer"},{"l":"wtf.choco.veinminer.api"},{"l":"wtf.choco.veinminer.api.event"},{"l":"wtf.choco.veinminer.data"},{"l":"wtf.choco.veinminer.pattern"},{"l":"wtf.choco.veinminer.tool"},{"l":"wtf.choco.veinminer.tool.template"}]
