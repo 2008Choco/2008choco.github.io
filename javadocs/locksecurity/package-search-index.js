aed30c4d7a519c201d490cc14458e6e6266e22dc
Parker Hawke <hawkeboyz2@hotmail.com>
2020-07-09 09:37:11 -0400

    Update LockSecurity Javadocs to 3.0.0

---
+++ javadocs/locksecurity/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"wtf.choco.locksecurity.api"},{"l":"wtf.choco.locksecurity.api.block"},{"l":"wtf.choco.locksecurity.api.event.block"},{"l":"wtf.choco.locksecurity.api.event.key"},{"l":"wtf.choco.locksecurity.api.key"},{"l":"wtf.choco.locksecurity.api.player"}]
