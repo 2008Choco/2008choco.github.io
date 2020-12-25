4beaf3dcad873935925fb23a94d8f0cf5ffe7789
Parker Hawke <hawkeboyz2@hotmail.com>
2020-12-25 16:25:51 -0500

    Push the Alchema Javadocs (1.0.0.0)

---
+++ javadocs/alchema/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"wtf.choco.alchema"},{"l":"wtf.choco.alchema.api.event"},{"l":"wtf.choco.alchema.api.event.entity"},{"l":"wtf.choco.alchema.cauldron"},{"l":"wtf.choco.alchema.crafting"},{"l":"wtf.choco.alchema.util"}];updateSearchResults();
