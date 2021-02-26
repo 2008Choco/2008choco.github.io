69d6d8c3e7957aa67d44a058bf72e2d47e4ef84d
Parker Hawke <hawkeboyz2@hotmail.com>
2021-02-26 10:50:34 -0500

    Fix missing Javadocs for Alchema 1.1.2

--- javadocs/alchema/package-search-index.js
+++ javadocs/alchema/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"wtf.choco.alchema.crafting"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"wtf.choco.alchema.api.event"},{"l":"wtf.choco.alchema.api.event.entity"},{"l":"wtf.choco.alchema.api.event.player"},{"l":"wtf.choco.alchema.cauldron"},{"l":"wtf.choco.alchema.config"},{"l":"wtf.choco.alchema.crafting"},{"l":"wtf.choco.alchema.persistence"},{"l":"wtf.choco.alchema.util"}];updateSearchResults();
