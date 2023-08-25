b0bb55e2e162e64926ce20ede5ccb254d17e5657
Parker Hawke <hawkeboyz2@hotmail.com>
2023-08-25 10:57:58 -0400

    Add alt text where applicable

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+This is my personal website. You are welcome to contribute to it if you feel you can improve the website in some way. I do not claim to be a professional website developer, so any help is appreciated. This website is static and makes use of HTML, CSS, JavaScript, jQuery, and Bootstrap.
+
 Domain: https://choco.wtf
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 const BIRTHDAY = new Date(1999, 10, 14)
 const MILLISECONDS_IN_ONE_YEAR = 3.154e+10
 
-const PREMIUM_RESOURCE_HTML_SUFFIX = `<span class="badge bg-success ml-2" title="Premium" style="color: white; font-weight: normal;">$</span>`
+const PREMIUM_RESOURCE_HTML_SUFFIX = `
+<span class="badge bg-success ml-2" title="Premium" style="color: white; font-weight: normal;">$</span>
+`
 
 $(document).ready(() => {
     // Age injection
@@ -17,22 +19,27 @@ $(document).ready(() => {
     // Generate plugin cards
     createPluginCard("veinminer", "VeinMiner", 
         "Mine an entire vein of ores by breaking only one source block. Highly configurable including custom tools, block lists and block aliasing.",
+        "A player using vein miner on a collection of diamond ores.",
         12038
     )
     createPluginCard("dragoneggdrop", "DragonEggDrop - Revival",
         "Custom YAML-based Ender Dragon templates, loot tables and infinite automatic Ender Dragon respawns (with countdowns!).",
+        "The Minecraft Ender Dragon death animation with a flame particle animation.",
         35570
     )
     createPluginCard("alchemicalarrows", "AlchemicalArrows", 
         "Special arrows with unique effects and abilities both in-world and against players and mobs. Perfect for diverse PvP environments and servers.",
+        "A player shooting various alchemical arrows at a dummy.",
         11693
     )
     createPluginCard("locksecurity", "LockSecurity 2", 
         "Lock chests, doors and other blocks using physical keys. Useful for players as container protection and admins for custom maps!",
+        "A player locking a Minecraft chest using a key.",
         81282, true
     )
     createPluginCard("alchema", "Alchema",
         "Integrates the world of brewery, witchcraft and magic into Minecraft without being intrusive on the vanilla feel of the game.",
+        "A player dropping materials into a bubbling cauldron to craft a new item.",
         87078
     )
 
@@ -54,11 +61,12 @@ $(document).ready(() => {
     )
 })
 
-function createPluginCard(pluginId, title, description, spigotResourceId, premium = false) {
+function createPluginCard(pluginId, title, description, altText, spigotResourceId, premium = false) {
     let pluginHTMLTemplate = $("#plugin-card-template").html()
         .replace("{{plugin_id}}", pluginId.toLowerCase())
         .replace("{{title}}", !premium ? title : `${title} ${PREMIUM_RESOURCE_HTML_SUFFIX}`)
         .replace("{{description}}", description)
+        .replace("{{alt_text}}", altText)
         .replace("{{url_github}}", `https://github.com/2008Choco/${pluginId}`)
         .replace("{{url_javadocs}}", `./javadocs/${pluginId}/index.html`)
         .replace("{{url_spigotmc}}", `https://spigotmc.org/resources/${spigotResourceId}`)
